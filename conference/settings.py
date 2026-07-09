7ba0b0329ebd7b174ebc26d5647aaf6fa5d8dde2
Clement <clementcrownrise@gmail.com>
2026-07-09 16:26:45 +0100

    first deployment

--- conference/settings.py
+++ conference/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 import dj_database_url
 load_dotenv()
 #load_dotenv(BASE_DIR / ".env")
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
