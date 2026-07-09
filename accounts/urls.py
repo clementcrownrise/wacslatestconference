3b558e97bc56e715c4df64cff7ef6c7b2f527381
Clement <clementcrownrise@gmail.com>
2026-07-09 19:39:20 +0100

    cursas

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views 
-from .views import create_super_admin
+#from .views import create_super_admin
 
 urlpatterns = [
     path('', views.login, name='login'),
